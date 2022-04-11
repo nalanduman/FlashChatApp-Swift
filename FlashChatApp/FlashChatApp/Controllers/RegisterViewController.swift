86ca4842ef470147c1e65cd507655fa024d6ac67
Nalan Duman <dumann16@itu.edu.tr>
2022-04-11 13:30:35 +0300

    Added createUser Function

--- FlashChatApp/FlashChatApp/Controllers/RegisterViewController.swift
+++ FlashChatApp/FlashChatApp/Controllers/RegisterViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class RegisterViewController: UIViewController {
 
@@ -19,6 +20,15 @@ class RegisterViewController: UIViewController {
     }
     
     @IBAction func registerPressed(_ sender: UIButton) {
+        if let email = emailTextField.text, let password = passwordTextField.text {
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                if let e = error {
+                    print(e.localizedDescription)
+                } else {
+                    self.performSegue(withIdentifier: "RegisterToChat", sender: self)
+                }
+            }
+        }
     }
 
 }
