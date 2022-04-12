f4f49c432f15882156d20d8820b88ff753568045
Nalan Duman <dumann16@itu.edu.tr>
2022-04-12 12:59:20 +0300

    Created MessageCell

--- FlashChatApp/FlashChatApp/Controllers/ChatViewController.swift
+++ FlashChatApp/FlashChatApp/Controllers/ChatViewController.swift
@@ -13,11 +13,21 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextField: UITextField!
     
+    var messages: [Message] = [
+        Message(sender: "nalan@gmail.com", body: "Hey!"),
+        Message(sender: "duman@gmail.com", body: "Hello!"),
+        Message(sender: "nalan@gmail.com", body: "What's up?")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        tableView.dataSource = self
+        
         title = K.appName
         navigationItem.hidesBackButton = true
+        
+        tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
@@ -30,5 +40,17 @@ class ChatViewController: UIViewController {
         } catch let e as NSError  {
             print(e.localizedDescription)
         }
+    }
+}
+
+extension ChatViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
+        cell.label.text = messages[indexPath.row].body
+        return cell
     }
 }
---
+++ FlashChatApp/FlashChatApp/Message.swift
@@ -0,0 +1,13 @@
+//
+//  Message.swift
+//  FlashChatApp
+//
+//  Created by Nalan Duman on 11.04.2022.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
